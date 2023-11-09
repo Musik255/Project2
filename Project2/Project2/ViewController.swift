cf1a897c793a1b2fed72c0e7591dac2f820c046a
P ch <pkchvyrov@vk.com>
2023-11-09 19:33:23 +0300

    added randomizer for pictures and variable for correct answer

--- Project2/Project2/ViewController.swift
+++ Project2/Project2/ViewController.swift
@@ -11,7 +11,7 @@ class ViewController: UIViewController {
     
     var countries = [String]()
     var score = 0
-    
+    var correctAnswer = 0
 
 
     @IBOutlet weak var button1: UIButton!
@@ -49,9 +49,12 @@ class ViewController: UIViewController {
         askQuestion()
     }
     func askQuestion() {
+        
         button1.setImage(UIImage(named: countries[0]), for: .normal)
         button2.setImage(UIImage(named: countries[1]), for: .normal)
         button3.setImage(UIImage(named: countries[2]), for: .normal)
+        correctAnswer = Int.random(in: 0...2)
+        title = countries[correctAnswer].uppercased()
     }
 
 
